b38eece1c818cfb74d1f2de3dd200a0c19c9bbfb
PEREZRA <rafael.perez@oocl.com>
2022-08-18 11:37:51 +0800

    added js files

---
+++ introObject.js
@@ -0,0 +1,12 @@
+
+var fruit = {
+apple: 20,
+pear: 20,
+peach: 10
+};
+
+var sum = fruit.apple + fruit. pear + fruit.peach;
+document.write(sum);
+
+
+
---
+++ introObjectName.js
@@ -0,0 +1,3 @@
+var user = {
+	surname : Mike
+}
